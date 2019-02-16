b7a02226f061b984af191194b0e64e29ac8d59d2
Eric Downs <eric@grainandmortar.com>
2019-02-16 17:16:08 -0600

    Theme progoress

---
+++ assets/gereral.js
@@ -0,0 +1,13 @@
+(function($){
+    $(function() {  //document.ready
+
+    	$('.see-more').on('click', function(c){
+    		$('.section-more-text-wrapper').addClass('open');
+
+
+    	});
+
+    //Functions, Plugins, Etc.. Here
+    //(does not wait for DOM READY STATE) 
+
+})(jQuery);
